90965dba7b36b0fac998f03c16ea82e5951caec0
Husniddinbek <hsopoyev11@gmail.com>
2023-07-19 15:56:57 +0500

    gsducgedi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+        User user = new User();
+        user.setName("Husniddin");
+        System.out.println("user = " + user);
     }
 }
